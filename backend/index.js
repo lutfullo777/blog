df011c07faf8579431b1a579c2a88643724f6624
Lutfullo <munavarovlutfullo3@gmail.com>
2021-06-21 18:32:22 +0500

    committed

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,7 @@ const path = require('path')
 const connectDB = require('./config/mongoDB');
 
 
+
 dotenv.config()
 connectDB()
 
@@ -34,6 +35,16 @@ app.use('/admin/upload',require('./admin/upload'))
 
 const dirname = path.resolve()
 app.use('/uploads', express.static(path.join(dirname, '/uploads')))
+
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static(path.join(__dirname, "/frontend/build")))
+
+  app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "../frontend", "build", "index.html")))
+}else{
+  app.get("/", (req, res) =>{
+    res.send("API is running....")
+  })
+}
 
 
 const PORT = process.env.PORT || 5000
