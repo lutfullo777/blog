42aad097011f40e15dc354c906e0a56c09ce03c7
Lutfullo <munavarovlutfullo3@gmail.com>
2021-06-22 21:07:06 +0500

    prepare to deployment

--- backend/index.js
+++ backend/index.js
@@ -37,9 +37,9 @@ const dirname = path.resolve()
 app.use('/uploads', express.static(path.join(dirname, '/uploads')))
 
 if(process.env.NODE_ENV === 'production'){
-  app.use(express.static(path.join(__dirname, "/frontend/build")))
+  app.use(express.static(path.join(__dirname, '../frontend/build')))
 
-  app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "../frontend", "build", "index.html")))
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../frontend', 'build', 'index.html')))
 }else{
   app.get("/", (req, res) =>{
     res.send("API is running....")
